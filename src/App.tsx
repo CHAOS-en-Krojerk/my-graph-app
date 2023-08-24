0ca130c3e0f5a6a092c25412c81a37a9b068c78a
zemach jacob <45815373+CHAOS-en-Krojerk@users.noreply.github.com>
2023-08-24 15:49:02 +0300

    Same

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,147 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactApexChart from 'react-apexcharts';
 
-function App() {
+export default function App() {
+  const initialSeries:any = [
+    {
+      name: 'Series 1',
+      data: [80, 50, 30, 40, 100, 20],
+    },
+    {
+      name: 'Series 2',
+      data: [20, 30, 40, 80, 20, 80],
+    },
+    {
+      name: 'Series 3',
+      data: [44, 76, 78, 13, 43, 10],
+    }]
+  const graphOptions:any = {
+    chart: {
+      width: 500,
+      type: 'radar',
+      dropShadow: {
+        enabled: true,
+        blur: 1,
+        left: 1,
+        top: 1
+      }
+    },
+    colors:['#F44336', '#E91E63', '#9C27B0'],//colors of series
+    title: {
+      text: 'Radar Chart - Multi Series',
+      style: {
+        color: '#e7e7e7'
+      }
+    },
+    stroke: {
+      width: 2
+    },
+    fill: {
+      opacity: 0.1
+    },
+    markers: {
+      size: 0
+    },
+    xaxis: {
+      categories: ['2011', '2012', '2013', '2014', '2015', '2016'],
+      labels: {
+        show: true,
+        style: {
+          colors: ["#a8a8a8", "#a8a8a8", "#a8a8a8", "#a8a8a8", "#a8a8a8", "#e7e7e7",],
+          fontSize: "11px",
+          fontFamily: 'Arial',
+        }
+      }
+    },
+    legend: {
+      labels: {
+        colors: '#a8a8a8',
+        useSeriesColors: false
+    },
+    }
+  };
+  
+  const initialData: any = {
+
+    series: initialSeries,
+    options: graphOptions,
+  };
+  const [graphData, setGraphData] = useState<any>(initialData);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+        <button onClick={()=>{
+          const newSeries = [
+            {
+              name: 'Series 1',
+              data: [80, 50, 30, 40, 100, 20],
+            },
+            {
+              name: 'Series 2',
+              data: [20, 30, 40, 80, 20, 80],
+            },
+            {
+              name: 'Series 3',
+              data: [44, 76, 78, 13, 43, 10],
+            }];
+            const newData: any = {
+              series: newSeries,
+              options: graphOptions,
+            }
+            setGraphData(newData);
+        }}>Set data A</button>
+        &nbsp;
+        <button onClick={()=>{
+          const newSeries = [
+            {
+              name: 'Series 1',
+              data: [90, 10, 75, 20, 55, 36],
+            },
+            {
+              name: 'Series 2',
+              data: [100, 90, 10, 63, 4, 40],
+            },
+            {
+              name: 'Series 3',
+              data: [86, 20, 64, 0, 95, 100],
+            }];
+            const newData: any = {
+              series: newSeries,
+              options: graphOptions,
+            }
+            setGraphData(newData);
+        }}>Set data B</button>
+        &nbsp;
+        <button onClick={()=>{
+          const newSeries = [
+            {
+              name: 'Series 1',
+              data: [12, 24, 56, 97, 12, 34],
+            },
+            {
+              name: 'Series 2',
+              data: [44, 3, 79, 45, 40, 60],
+            },
+            {
+              name: 'Series 3',
+              data: [20, 43, 45, 70, 26, 10],
+            }];
+            const newData: any = {
+              series: newSeries,
+              options: graphOptions,
+            }
+            setGraphData(newData);
+        }}>Set data C</button>
+        </div>
+        <div  style={{display: 'flex', flexDirection: 'row'}}>
+
+        </div>
+        <ReactApexChart options={graphData.options} series={graphData.series} type="radar" width={500} height={500} />
       </header>
     </div>
   );
 }
-
-export default App;
