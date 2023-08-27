5352725a081e86b5b8bfc458efbf5ffea8333c14
CHAOS-en-Krojerk <dancebk0@gmail.com>
2023-08-27 14:05:15 +0300

    Added further customization options

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import ReactApexChart from 'react-apexcharts';
 
-export default function App() {
-  const initialSeries:any = [
+export default function App()
+{
+  const initialSeries: any = [
     {
       name: 'Series 1',
       data: [80, 50, 30, 40, 100, 20],
@@ -17,7 +18,7 @@ export default function App() {
       name: 'Series 3',
       data: [44, 76, 78, 13, 43, 10],
     }]
-  const graphOptions:any = {
+  const graphOptions: any = {
     chart: {
       width: 500,
       type: 'radar',
@@ -28,7 +29,7 @@ export default function App() {
         top: 1
       }
     },
-    colors:['#F44336', '#E91E63', '#9C27B0'],//colors of series
+    colors: ['#F44336', '#E91E63', '#9C27B0'],//colors of series
     title: {
       text: 'Radar Chart - Multi Series',
       style: {
@@ -41,17 +42,76 @@ export default function App() {
     fill: {
       opacity: 0.1
     },
-    markers: {
-      size: 0
+    tooltip: {
+      fillSeriesColor: true,
+      theme: 'dark'
     },
+    markers: {
+      size: 5,
+      hover: {
+        size: 10
+      }
+    },
+
+    dataLabels: {
+      enabled: true,
+      enabledOnSeries: undefined,
+      formatter: function (val:any, opts:any) {
+          return val
+      },
+      textAnchor: 'middle',
+      distributed: false,
+      offsetX: 0,
+      offsetY: 0,
+      style: {
+          fontSize: '10px',
+          fontFamily: 'Helvetica, Arial, sans-serif',
+          fontWeight: 'bold',
+          colors: undefined
+      },
+      background: {
+        enabled: true,
+        foreColor: '#fff',
+        padding: 4,
+        borderRadius: 2,
+        borderWidth: 1,
+        borderColor: '#fff',
+        opacity: 0.9,
+        dropShadow: {
+          enabled: false,
+          top: 1,
+          left: 1,
+          blur: 1,
+          color: '#000',
+          opacity: 0.45
+        }
+      },
+      dropShadow: {
+          enabled: false,
+          top: 1,
+          left: 1,
+          blur: 1,
+          color: '#000',
+          opacity: 0.45
+      }
+    },
+
     xaxis: {
       categories: ['2011', '2012', '2013', '2014', '2015', '2016'],
       labels: {
         show: true,
         style: {
-          colors: ["#a8a8a8", "#a8a8a8", "#a8a8a8", "#a8a8a8", "#a8a8a8", "#e7e7e7",],
+          colors: '#a8a8a8',
           fontSize: "11px",
           fontFamily: 'Arial',
+        }
+      }
+    },
+    yaxis: {
+      labels: {
+        show: true,
+        style: {
+          colors: "#e7e7e7"
         }
       }
     },
@@ -59,12 +119,11 @@ export default function App() {
       labels: {
         colors: '#a8a8a8',
         useSeriesColors: false
-    },
+      },
     }
   };
-  
-  const initialData: any = {
 
+  const initialData: any = {
     series: initialSeries,
     options: graphOptions,
   };
@@ -73,71 +132,74 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div style={{display: 'flex', flexDirection: 'row'}}>
-        <button onClick={()=>{
-          const newSeries = [
-            {
-              name: 'Series 1',
-              data: [80, 50, 30, 40, 100, 20],
-            },
-            {
-              name: 'Series 2',
-              data: [20, 30, 40, 80, 20, 80],
-            },
-            {
-              name: 'Series 3',
-              data: [44, 76, 78, 13, 43, 10],
-            }];
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <button onClick={() =>
+          {
+            const newSeries = [
+              {
+                name: 'Series 1',
+                data: [80, 50, 30, 40, 100, 20],
+              },
+              {
+                name: 'Series 2',
+                data: [20, 30, 40, 80, 20, 80],
+              },
+              {
+                name: 'Series 3',
+                data: [44, 76, 78, 13, 43, 10],
+              }];
             const newData: any = {
               series: newSeries,
               options: graphOptions,
             }
             setGraphData(newData);
-        }}>Set data A</button>
-        &nbsp;
-        <button onClick={()=>{
-          const newSeries = [
-            {
-              name: 'Series 1',
-              data: [90, 10, 75, 20, 55, 36],
-            },
-            {
-              name: 'Series 2',
-              data: [100, 90, 10, 63, 4, 40],
-            },
-            {
-              name: 'Series 3',
-              data: [86, 20, 64, 0, 95, 100],
-            }];
+          }}>Set data A</button>
+          &nbsp;
+          <button onClick={() =>
+          {
+            const newSeries = [
+              {
+                name: 'Series 1',
+                data: [90, 10, 75, 20, 55, 36],
+              },
+              {
+                name: 'Series 2',
+                data: [100, 90, 10, 63, 4, 40],
+              },
+              {
+                name: 'Series 3',
+                data: [86, 20, 64, 0, 95, 100],
+              }];
             const newData: any = {
               series: newSeries,
               options: graphOptions,
             }
             setGraphData(newData);
-        }}>Set data B</button>
-        &nbsp;
-        <button onClick={()=>{
-          const newSeries = [
-            {
-              name: 'Series 1',
-              data: [12, 24, 56, 97, 12, 34],
-            },
-            {
-              name: 'Series 2',
-              data: [44, 3, 79, 45, 40, 60],
-            },
-            {
-              name: 'Series 3',
-              data: [20, 43, 45, 70, 26, 10],
-            }];
+          }}>Set data B</button>
+          &nbsp;
+          <button onClick={() =>
+          {
+            const newSeries = [
+              {
+                name: 'Series 1',
+                data: [12, 24, 56, 97, 12, 34],
+              },
+              {
+                name: 'Series 2',
+                data: [44, 3, 79, 45, 40, 60],
+              },
+              {
+                name: 'Series 3',
+                data: [20, 43, 45, 70, 26, 10],
+              }];
             const newData: any = {
               series: newSeries,
               options: graphOptions,
             }
             setGraphData(newData);
-        }}>Set data C</button>
+          }}>Set data C</button>
         </div>
-        <div  style={{display: 'flex', flexDirection: 'row'}}>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
 
         </div>
         <ReactApexChart options={graphData.options} series={graphData.series} type="radar" width={500} height={500} />
